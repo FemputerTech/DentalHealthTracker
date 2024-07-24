2c61825cef76f4d70947bd4b944a38794f862d35
FemputerTech <mleicht92@gmail.com>
2024-07-24 10:36:40 -0700

    my config file wasn't setup to put the database.db file in the correct location

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,7 @@
+import os
+
 class Config:
+    BASE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
     SECRET_KEY = 'my-secure-secret-key'
-    SQLALCHEMY_DATABASE_URI = "sqlite:///dhmodel/database.db"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIRECTORY, '../website/database.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
