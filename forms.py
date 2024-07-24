13fb61c9e5b3c574df0da05fcc62b1c98e10964b
FemputerTech <mleicht92@gmail.com>
2024-07-23 19:00:24 -0700

    added a forms.py for all the auth forms

---
+++ forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, DateField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.widgets import TelInput
+import dhmodel
+
+
+class SignupForm(FlaskForm):
+    first_name = StringField(
+        validators=[DataRequired(), Length(min=1, max=40)],
+        render_kw={"placeholder": "First name"}
+    )
+    last_name = StringField(
+        validators=[DataRequired(), Length(min=1, max=80)],
+        render_kw={"placeholder": "Last name"}
+    )
+    email = StringField(
+        validators=[DataRequired(), Email()],
+        render_kw={"placeholder": "Email"}
+        )
+    dob = DateField(
+        validators=[DataRequired()],
+        format='%Y-%m-%d'
+    )
+    tel = StringField(
+        validators=[DataRequired()],
+        widget=TelInput(),
+        render_kw={"placeholder": "123-456-7890"}
+    ) 
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=8, max=20)],
+        render_kw={"placeholder": "Password"}
+    )
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")],
+        render_kw={"placeholder": "Confirm password"}
+    )
+    submit = SubmitField("Sign Up")
+
+    def validate_email(self, email):
+        users = dhmodel.get_user_model()
+        existing_user = users.query.filter_by(email=email.data).first()
+        
+        if existing_user:
+            raise ValidationError("That email already exists. Please choose a different one.")
