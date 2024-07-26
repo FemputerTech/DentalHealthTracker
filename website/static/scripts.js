53b9db971a29dc3d60496ad99bad3679a557c3c5
FemputerTech <mleicht92@gmail.com>
2024-07-26 13:16:23 -0700

    refactored how i'm handling transitions and responsiveness for the dashboard sidebar and main content area

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -12,17 +12,17 @@ function toggleSidebar() {
   const sidebarToggle = document.querySelector(".sidebar-toggle");
   const boxIcon = sidebarToggle.querySelector("box-icon");
   const navLabels = document.querySelectorAll(".nav-label");
-  const header = document.querySelector(".header-authenticated");
   const sidebar = document.querySelector(".sidebar");
+  const dashboard = document.querySelector(".main-dashboard");
 
   if (boxIcon.getAttribute("name") === "chevrons-right") {
     boxIcon.setAttribute("name", "chevrons-left");
     sidebar.classList.add("expanded");
-    header.classList.add("expanded");
+    dashboard.classList.add("expanded");
   } else {
     boxIcon.setAttribute("name", "chevrons-right");
     sidebar.classList.remove("expanded");
-    header.classList.remove("expanded");
+    dashboard.classList.remove("expanded");
   }
 
   navLabels.forEach((navLabel) => {
