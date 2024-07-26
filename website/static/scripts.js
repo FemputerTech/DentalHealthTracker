6a91e3d5951484267b0e3e03fef99cdaa9c217f3
FemputerTech <mleicht92@gmail.com>
2024-07-26 11:44:59 -0700

    wrote the toggleSidebar method

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -1,9 +1,35 @@
 function toggleNavList() {
-  navList = document.querySelector(".nav-list");
+  const navList = document.querySelector(".nav-list");
 
   if (navList.style.display === "") {
     navList.style.display = "flex";
   } else {
     navList.style.display = "";
   }
+}
+
+function toggleSidebar() {
+  const sidebarToggle = document.querySelector(".sidebar-toggle");
+  const boxIcon = sidebarToggle.querySelector("box-icon");
+  const navLabels = document.querySelectorAll(".nav-label");
+  const header = document.querySelector(".header-authenticated");
+  const sidebar = document.querySelector(".sidebar");
+
+  if (boxIcon.getAttribute("name") === "chevrons-right") {
+    boxIcon.setAttribute("name", "chevrons-left");
+    sidebar.classList.add("expanded");
+    header.classList.add("expanded");
+  } else {
+    boxIcon.setAttribute("name", "chevrons-right");
+    sidebar.classList.remove("expanded");
+    header.classList.remove("expanded");
+  }
+
+  navLabels.forEach((navLabel) => {
+    if (navLabel.style.display === "") {
+      navLabel.style.display = "flex";
+    } else {
+      navLabel.style.display = "";
+    }
+  });
 }
