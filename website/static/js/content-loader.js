1084d6031bb772866554d84857adddfce518fd3d
FemputerTech <mleicht92@gmail.com>
2024-07-27 16:09:18 -0700

    separated out the content loader component javscript

---
+++ website/static/js/content-loader.js
@@ -0,0 +1,39 @@
+class ContentLoader {
+  constructor() {
+    this.loadContentButtons = document.querySelectorAll(".label-button");
+    this.sidebarLinks = document.querySelectorAll(".sidebar .nav-link.content");
+    this.init();
+  }
+
+  init() {
+    this.loadContentButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const section = button.getAttribute("data-section");
+        this.loadContent(section);
+      });
+    });
+    this.sidebarLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        const section = link.getAttribute("data-section");
+        this.loadContent(section);
+      });
+    });
+  }
+
+  async loadContent(section) {
+    fetch(`/dashboard/${section}`)
+      .then((response) => response.text())
+      .then((html) => {
+        document.querySelector(".dashboard-content").innerHTML = html;
+        localStorage.setItem("currentDashboardSection", section);
+      })
+      .catch((error) => console.error("Error loading content:", error));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const contentLoader = new ContentLoader();
+  const savedDashboardSection =
+    localStorage.getItem("currentDashboardSection") || "overview";
+  contentLoader.loadContent(savedDashboardSection);
+});
