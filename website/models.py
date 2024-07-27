648adfe7a3952f717fe67719221d7e36e40216d9
FemputerTech <mleicht92@gmail.com>
2024-07-26 17:34:10 -0700

    added a model for messages

--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
 from .extensions import db
 from flask_login import UserMixin
+import datetime
 
 
 class User(db.Model, UserMixin):
@@ -12,3 +13,11 @@ class User(db.Model, UserMixin):
     dob = db.Column(db.Date, nullable=False)
     tel = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    role = db.Column(db.String(10), nullable=False)  # 'user' or 'bot'
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime)
