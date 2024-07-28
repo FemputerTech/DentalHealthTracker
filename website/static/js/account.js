5f40d32e8a20417b5e099cd97cec79918b169b3b
FemputerTech <mleicht92@gmail.com>
2024-07-28 15:19:12 -0700

    displayed the account information

--- website/static/js/account.js
+++ website/static/js/account.js
@@ -1,1 +1,26 @@
-class Account {}
+class Account {
+  constructor(user) {
+    this.user = user;
+    this.init();
+  }
+
+  init() {
+    this.displayUser();
+  }
+
+  displayUser() {
+    const accountNumber = document.querySelector(".user-account");
+    const name = document.querySelector(".user-name");
+    const email = document.querySelector(".user-email");
+    const dob = document.querySelector(".user-dob");
+    const tel = document.querySelector(".user-tel");
+    const dentist = document.querySelector(".user-dentist");
+
+    accountNumber.textContent = `Account: ${this.user.account_number}`;
+    name.textContent = `${this.user.first_name} ${this.user.last_name}`;
+    email.textContent = this.user.email;
+    dob.textContent = this.user.dob;
+    tel.textContent = this.user.tel;
+    dentist.textContent = this.user.dentist;
+  }
+}
