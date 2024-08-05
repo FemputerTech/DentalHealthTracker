b79320b0eae405776207f8b6089a5d1ae92c535f
FemputerTech <mleicht92@gmail.com>
2024-08-05 10:25:03 -0700

    made user_id a foreign key in Chat model

--- website/models.py
+++ website/models.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin):
 
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     role = db.Column(db.String(10), nullable=False)  # 'user' or 'bot'
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.datetime.now())
