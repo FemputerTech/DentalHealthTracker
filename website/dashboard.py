9546843cd2c7aaaf427cf7ff7ec9a3d3f4f7c99a
FemputerTech <mleicht92@gmail.com>
2024-08-05 11:51:44 -0700

    added the post request for changing user telephone

--- website/dashboard.py
+++ website/dashboard.py
@@ -10,17 +10,20 @@ dash = Blueprint("dash", __name__)
 @login_required
 def dashboard():
     user_id = current_user.id
-    print("current primary dentist:", current_user.dentist_id)
     messages = Chat.query.filter_by(user_id=user_id).order_by(Chat.timestamp).all()
     dentists = Dentist.query.order_by(Dentist.id).all()
+    user_dentist = Dentist.query.get(current_user.dentist_id) if current_user.dentist_id else None
     formatted_messages = [{"role": message.role, "content": message.content} for message in messages]
 
     if request.method == "POST":
         new_dentist_id = request.form.get("dentist_id")
-        print("dentist id: ", new_dentist_id)
+        new_tel = request.form.get("tel")
         if new_dentist_id:
             current_user.dentist_id = new_dentist_id
             db.session.commit()
-            return redirect(url_for("dash.dashboard"))
+        if new_tel:
+            current_user.tel = new_tel
+            db.session.commit()
+        return redirect(url_for("dash.dashboard"))
         
-    return render_template("dashboard.html", user=current_user, messages=formatted_messages, dentists=dentists)
+    return render_template("dashboard.html", user=current_user, messages=formatted_messages, dentists=dentists, user_dentist=user_dentist)
