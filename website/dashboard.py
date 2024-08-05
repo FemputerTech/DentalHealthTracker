edfa24939b7c8532955ef747bffc3ebfdcc12a4b
FemputerTech <mleicht92@gmail.com>
2024-08-05 11:44:09 -0700

    added a post request behavior to update the new dentist

--- website/dashboard.py
+++ website/dashboard.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
-from .models import Chat
+from .models import Chat, Dentist
+from .extensions import db
 
 
 dash = Blueprint("dash", __name__)
@@ -9,6 +10,17 @@ dash = Blueprint("dash", __name__)
 @login_required
 def dashboard():
     user_id = current_user.id
+    print("current primary dentist:", current_user.dentist_id)
     messages = Chat.query.filter_by(user_id=user_id).order_by(Chat.timestamp).all()
+    dentists = Dentist.query.order_by(Dentist.id).all()
     formatted_messages = [{"role": message.role, "content": message.content} for message in messages]
-    return render_template("dashboard.html", user=current_user, messages=formatted_messages)
+
+    if request.method == "POST":
+        new_dentist_id = request.form.get("dentist_id")
+        print("dentist id: ", new_dentist_id)
+        if new_dentist_id:
+            current_user.dentist_id = new_dentist_id
+            db.session.commit()
+            return redirect(url_for("dash.dashboard"))
+        
+    return render_template("dashboard.html", user=current_user, messages=formatted_messages, dentists=dentists)
